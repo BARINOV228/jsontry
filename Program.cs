54e9a64e755f46cd74b341111504c230f56ad640
BARINOV <gurya2405@gmail.com>
2023-09-25 18:49:50 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using jsontry;
+using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+//User user = new User()
+//{
+//    Id = 2,
+//    Name = "Gus",
+//    UserName = "Antonelle",
+//    Email = "email@gmail.com",
+//    Adress = new Adress() 
+//    {
+//        Street = "Victor Plains",
+//        City = "Tokio", ZipCode = "90566-7771", 
+//        geo = new Geo()
+//        {
+//            Lat = "-47.2345",
+//            Lng = "-34.1324"
+//        } 
+//    },
+//    Phone = "+998 88 888 88 85",
+//    Website = "https://poe.com/chat/2kfs7ineznqi2oiezyu",
+//    Company = new Company()
+//    {
+//        Name = "Apple", CatchPhrase = "Productivity", Bs = "Lol something"
+//    }
+//};
+//string json = JsonConvert.SerializeObject(user, Formatting.Indented);
+
+
+var person = new Person()
+{
+    Name = "Jason",
+    Age = 31,
+    City = "Sweden",
+    Place = new Place() { Home = "Flat"}
+};
+
+
+var serializer = new XmlSerializer(typeof(Person));
+using (TextWriter writer = new StringWriter())
+{
+    serializer.Serialize(writer, person);
+    string xml = writer.ToString();
+
+    Console.WriteLine("XML: " + xml);
+}
+
+
+
+
+
+
+
+
+
